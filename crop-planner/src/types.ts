07df6d2e37a8bf98490fdcbfca716f13901cf07e
Artem Kulakov <artem.kulakov@gmail.com>
2021-08-18 00:57:24 +0300

    Humus balance is fetched at page load for all fields

--- crop-planner/src/types.ts
+++ crop-planner/src/types.ts
@@ -10,4 +10,5 @@ export type Field = {
   name: string,
   area: number,
   crops: Array<SeasonalCrop>,
+  balance: number,
 }
--- soil-service/app/services/fields_service.rb
+++ soil-service/app/services/fields_service.rb
@@ -2,44 +2,64 @@
 class FieldsService
   include Singleton
 
+  FIELDS = [
+    {
+      id: 1,
+      name: 'Mäeotsa',
+      area: 0.93,
+      crops: [
+        { year: 2020, crop: CropsService::WINTER_WHEAT },
+        { year: 2021, crop: CropsService::OATS },
+        { year: 2022, crop: CropsService::WINTER_WHEAT },
+        { year: 2023, crop: CropsService::OATS },
+        { year: 2024, crop: CropsService::WINTER_WHEAT },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Tiigimanu',
+      area: 3.14,
+      crops: [
+        { year: 2020, crop: CropsService::SPRING_WHEAT },
+        { year: 2021, crop: CropsService::OATS },
+        { year: 2022, crop: CropsService::RED_CLOVER },
+        { year: 2023, crop: CropsService::WINTER_WHEAT },
+        { year: 2024, crop: CropsService::BROAD_BEAN },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Künkatagune',
+      area: 5.18,
+      crops: [
+        { year: 2020, crop: CropsService::SPRING_WHEAT },
+        { year: 2021, crop: CropsService::SPRING_WHEAT },
+        { year: 2022, crop: CropsService::SPRING_WHEAT },
+        { year: 2023, crop: CropsService::SPRING_WHEAT },
+        { year: 2024, crop: CropsService::SPRING_WHEAT },
+      ],
+    },
+  ].freeze
+
   def fetch_fields
-    [
-      {
-        id: 1,
-        name: 'Mäeotsa',
-        area: 0.93,
-        crops: [
-          { year: 2020, crop: CropsService::WINTER_WHEAT },
-          { year: 2021, crop: CropsService::OATS },
-          { year: 2022, crop: CropsService::WINTER_WHEAT },
-          { year: 2023, crop: CropsService::OATS },
-          { year: 2024, crop: CropsService::WINTER_WHEAT },
-        ],
-      },
-      {
-        id: 2,
-        name: 'Tiigimanu',
-        area: 3.14,
-        crops: [
-          { year: 2020, crop: CropsService::SPRING_WHEAT },
-          { year: 2021, crop: CropsService::OATS },
-          { year: 2022, crop: CropsService::RED_CLOVER },
-          { year: 2023, crop: CropsService::WINTER_WHEAT },
-          { year: 2024, crop: CropsService::BROAD_BEAN },
-        ],
-      },
-      {
-        id: 3,
-        name: 'Künkatagune',
-        area: 5.18,
-        crops: [
-          { year: 2020, crop: CropsService::SPRING_WHEAT },
-          { year: 2021, crop: CropsService::SPRING_WHEAT },
-          { year: 2022, crop: CropsService::SPRING_WHEAT },
-          { year: 2023, crop: CropsService::SPRING_WHEAT },
-          { year: 2024, crop: CropsService::SPRING_WHEAT },
-        ],
-      },
-    ]
+    result = []
+
+    FIELDS.each do |field|
+      humus_balance = 0
+      prior_year_crop_value = nil
+
+      field[:crops].each do |year|
+        factor = (year[:crop][:value] == prior_year_crop_value) ? 1.3 : 1.0
+        humus_balance += year[:crop][:humus_delta] * field[:area] * factor
+
+        prior_year_crop_value = year[:crop][:value]
+      end
+
+      field[:balance] = humus_balance.round(2)
+
+      result << field
+    end
+
+    result
   end
 end
